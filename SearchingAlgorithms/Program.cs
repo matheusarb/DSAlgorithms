dd5b9248f3d83a08ee17cdb425f1ffcf3b77f907
Matheus <mat.araujoribeiro@gmail.com>
2023-11-09 11:36:21 -0300

    [refactor] binarySearch

--- SearchingAlgorithms/Program.cs
+++ SearchingAlgorithms/Program.cs
@@ -1,37 +1,91 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Security.AccessControl;
+
 Console.WriteLine("-------------SEARCHING ALGORITHMS------------");
 
-var arr1 = new int[] { 84, 21, 47, 96, 34, 120 };
+var arr1 = new int[] { 21, 34, 47, 84, 96, 120 };
 
-//foreach (var item in arr1)
-//{
-//    Console.Write(item+ " ");
-//}
+Console.WriteLine(BinarySearch(arr1, 6, 96));
+Console.WriteLine(MyBinarySearch(arr1, 96));
 
-Console.WriteLine(LinearSearch(arr1, 6, 21));
-MinhaLinearSeach(arr1, 21);
 
-//Linear Seach do curso
-static int LinearSearch(int[] A, int ALength, int key)
+//------------------------- BINARY SEARCH -----------------------
+// Função do curso
+static int BinarySearch(int[] Arr, int ALength, int Key)
 {
-    var index = 0;
-    while(index < ALength)
+    //inicializar duas variáveis: var L para o primeiro index
+    //var R para o último index. Duas variáveis para mapear o index
+
+    var Left = 0;
+    var Right = ALength - 1;
+
+    //verificar se o L é menor ou igual que o R
+    while(Left <= Right)
     {
-        if (A[index] == key)
-            //return index; //achar o index
-            return key; //achar o value
-            //return A[index]; //achar o value 2
-        index++;
+        var middle = (Left + Right) / 2;
+        if (Key == Arr[middle])
+            return middle;
+        else if (Key < Arr[middle])
+            Right = middle - 1;
+        else if (Key > Arr[middle])
+            Left = middle + 1;
+    }
+    return -1;
+}
+// Minha função
+static int MyBinarySearch(int[] arr, int value)
+{
+    var left = 0;
+    var right = arr.Length - 1;
+
+    while (left <= right)
+    {
+        var middle = (left + right) / 2;
+        if (value == arr[middle])
+        {
+            return arr[middle];
+        }
+        else if (value < arr[middle])
+        {
+            right = middle - 1;
+        }
+        else if (value > arr[middle])
+        {
+            left = middle + 1;
+        }
     }
     return -1;
 }
 
+static void Exibir(int[] arr)
+{
+    foreach(var i in arr)
+    {
+        Console.Write(i+" ");
+    }
+}
+
+//-------------------------LINEAR SEACH--------------------------
+//Linear Seach do curso
+static int LinearSearch(int[] A, int ALength, int key)
+{
+    var index = 0;
+    while (index < ALength)
+    {
+        if (A[index] == key)
+            //return index; //achar o index
+            return key; //achar o value
+                        //return A[index]; //achar o value 2
+        index++;
+    }
+    return -1;
+}
 //Minha LinearSearch
 static void MinhaLinearSeach(int[] arr, int value)
 {
     var index = 0;
     var found = false;
-    while(index < arr.Length)
+    while (index < arr.Length)
     {
         var valArr = arr[index];
         //Console.WriteLine($"Iteração com o valor {arr[index]}");
@@ -43,6 +97,6 @@ static void MinhaLinearSeach(int[] arr, int value)
         }
         index++;
     }
-        if (!found)
-            Console.WriteLine("Não existe o valor na array informada");
+    if (!found)
+        Console.WriteLine("Não existe o valor na array informada");
 }
