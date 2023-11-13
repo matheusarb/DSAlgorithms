5e185ed55e20726ae52d16545a99cf4e27dcba30
Matheus <mat.araujoribeiro@gmail.com>
2023-11-13 00:23:39 -0300

    [add] bubbleSort

--- SortingAlgorithms/Program.cs
+++ SortingAlgorithms/Program.cs
@@ -7,5 +7,39 @@
 //{ Count Sort, Bucket Sort, Radix Sort
 
 
-//1. ---SELECTION SORT-----------
+//1. ----------SELECTION SORT-----------
 //Escolher o menor elemento na coleção e posicioná-lo no lugar apropriado (início). o mesmo procedimento será feito com todos os elementos
+
+
+//2. ----------BUBBLE SORT--------------
+//IDEIA: Compara o index atual com o index seguinte; se for menor, trocam de lugar, senão segue para a próxima comparação (n0 comparar com n1, dps n1 com n2, n2 com n3...)
+int[] arr = new int[5] {2, 4, 5, 1, 7};
+MybubbleSort(arr);
+
+static void MybubbleSort(int[] arr)
+{
+    var length = arr.Length;
+    int i, j, temp;
+    for(i = 0; i < arr.Length-1; i++)
+    {
+        for(j = 0; j < arr.Length-1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    Exibir(arr);
+}
+
+static void Exibir(int[] arr)
+{
+    foreach(int i in arr)
+    {
+        Console.Write(i+" ");
+    }
+}
