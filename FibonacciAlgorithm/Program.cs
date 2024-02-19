6a160b9da971d4434ffd3b12cf54898968807be8
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-02-19 15:38:41 -0300

    fiboCheck

--- FibonacciAlgorithm/Program.cs
+++ FibonacciAlgorithm/Program.cs
@@ -6,68 +6,91 @@ List<int> fiboSeq = new List<int>();
 int i = 1;
 while (i <= 50)
 {
-    if (isFibo(i))
+    // if (isFibo(i))
     {
         fiboSeq.Add(i);
     }
     i++;
 }
-Exibir(fiboSeq);
+// Exibir(fiboSeq);
 
-//fiboSequence2(5);
-//ambas erradas eu acho
-static bool fibonacciSequence1(int n)
+// Create and check
+static bool CreateAndCheck(int num)
 {
-    List<int> fibSeq = new List<int>();
-    var n1 = 0;
-    var n2 = 1;
-    fibSeq.Add(n1);
-    fibSeq.Add(n2);
+//     int j = 0;
+//     int k = 1, l = 1;
+    var fibo = new List<int>{ 0, 1, 1 };
 
-    for (var i = 2; i < fibSeq.Count; i++)
+    if(num == 0 || num == 1 || num == 2)
+        return true;
+
+    for(var i=3; i <= num; i++)
     {
-        fibSeq[i] = fibSeq[i - 1] + fibSeq[i - 2];
+        fibo[i] = fibo[i - 1] + fibo[i - 2];
+        fibo.Add(fibo[i]);
         i++;
-        if (n == fibSeq[i])
-        {
-            return true;
-        }
     }
+    
+    if(fibo.Contains(num))
+        return true;
 
     return false;
 }
 
-static void fiboSequence2(int nthTerm)
-{
-    int n1 = 0;
-    int n2 = 1;
-    int nthNumber;
+//fiboSequence2(5);
+//ambas erradas eu acho
+// static bool fibonacciSequence1(int n)
+// {
+//     List<int> fibSeq = new List<int>();
+//     var n1 = 0;
+//     var n2 = 1;
+//     fibSeq.Add(n1);
+//     fibSeq.Add(n2);
 
-        for(int i = 0; i < nthTerm; i++)
-        {
-            Console.Write(n1 + " ");
-            nthNumber = n1 + n2;
-            n1 = n2;
-            n2 = nthNumber;
-        }
-}
+//     for (var i = 2; i < fibSeq.Count; i++)
+//     {
+//         fibSeq[i] = fibSeq[i - 1] + fibSeq[i - 2];
+//         i++;
+//         if (n == fibSeq[i])
+//         {
+//             return true;
+//         }
+//     }
 
-//3a tentativa
-static bool isPerfSq(int n)
-{
-    var s = (int)Math.Sqrt(n);
-    return (s * s == n);
-}
+//     return false;
+// }
 
-static bool isFibo(int n)
-{
-    return isPerfSq(5 * n * n + 4) || isPerfSq(5 * n * n - 4);
-}
+// static void fiboSequence2(int nthTerm)
+// {
+//     int n1 = 0;
+//     int n2 = 1;
+//     int nthNumber;
 
-static void Exibir(List<int> list)
-{
-    foreach (var item in list)
-    {
-        Console.Write(item+" ");
-    }
-}
+//         for(int i = 0; i < nthTerm; i++)
+//         {
+//             Console.Write(n1 + " ");
+//             nthNumber = n1 + n2;
+//             n1 = n2;
+//             n2 = nthNumber;
+//         }
+// }
+
+// //3a tentativa
+// static bool isPerfSq(int n)
+// {
+//     var s = (int)Math.Sqrt(n);
+//     return (s * s == n);
+// }
+
+// static bool isFibo(int n)
+// {
+//     return isPerfSq(5 * n * n + 4) || isPerfSq(5 * n * n - 4);
+// }
+
+// static void Exibir(List<int> list)
+// {
+//     foreach (var item in list)
+//     {
+//         Console.Write(item+" ");
+//     }
+// }
