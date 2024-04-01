c773289cb9f5f7c01a67d92ef5bf3708b553f727
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-04-01 12:35:57 -0300

    binary search

--- DSAlgorithms/Program.cs
+++ DSAlgorithms/Program.cs
@@ -1,5 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("DSAlgorithms no code here");
 
-HashCode g = new HashCode();
-System.Console.WriteLine(g);
+
+var list = new List<int> { 1, 3, 5, 7, 9, 11 };
+func(list, 1);
+static void func(List<int> list, int val)
+{
+    int min, max;
+    int guess = 0;
+    int middle = 0;
+
+    min = 0;
+    max = list.Count - 1;
+
+    while (min <= max)
+    {
+        middle = (min + max) / 2;
+        guess = list[middle];
+
+        if (guess == val)
+            break;
+        if (guess > val)
+            max = middle - 1;
+        else
+            min = middle + 1;
+    }
+    System.Console.WriteLine($"val {val} | position {middle} | guess {guess}");
+}
