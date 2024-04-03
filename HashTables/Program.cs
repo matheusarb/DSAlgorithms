594cd699f5c58fc0aeb6d630607f50cd6060e619
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-04-02 21:28:24 -0300

    add hash tables

--- HashTables/Program.cs
+++ HashTables/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("----------HASH TABLES----------");
+
+// Numa função Hash nós inserimos uma string e recebemos um número de volta, equivalente a string inserida
+    // 1. Consistência. A mesma string inserida deverá retornar o mesmo númoer
+    // 2. Deve mapear diferentes palavras para diferentes números
+//Uma função hash funciona através da função Hash somada a uma array
+
+// São úteis para:
+    // 1.mapear algum item em relação a outro
+    // 2.precisa pesquisar algo
+    // 3. Utilizar como cache
+
+
+Dictionary<string, decimal> estoqueMercado = new Dictionary<string, decimal>(){};
+estoqueMercado.Add("Maca", 3.23m);
+
+System.Console.WriteLine(estoqueMercado["Maca"]);
+
